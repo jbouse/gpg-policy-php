ade662d230666b62fae67f54549dae06a89145c6
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2022-02-20 23:16:57 -0500

    fix(deps): upgrade missing pieces
    Signed-off-by: Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Bref\SymfonyBridge\BrefKernel as BaseKernel;
+// use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 
 class Kernel extends BaseKernel
